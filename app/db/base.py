e909116b1e449d0e37eb131d745994d74e275333
hyo00000 <gywkdk80@gmail.com>
2025-06-14 23:42:13 +0900

    Modify base.py

--- app/db/base.py
+++ app/db/base.py
@@ -1,4 +1,4 @@
-from app.models.user import Admin
+from app.models.user import User
 from app.models.menu import Menu
 from app.models.order import Order
 from app.models.reservation import Reservation
